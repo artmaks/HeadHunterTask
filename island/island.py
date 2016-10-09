823e57210800b37fec29e33d4b2d8ad2d982f631
artmaks <temamksimov@gmail.com>
2016-10-09 14:00:13 +0300

    Добавлены комментарии

--- island/island.py
+++ island/island.py
@@ -3,20 +3,27 @@
 import numpy as np
 from numpy import sum
 
-# Проверяем, является ли клетка низиной.
-# Если не является => False
-# Если явлется => набор клеток являющихся низиной (частный случай, когда 1 клетка)
+# Проверяем, является ли клетка низиной
+# И все ее соседи с той же высотой (функция рекурентная)
+# Если не является (вода уходит) => False
+# Если явлется (вода остается) => набор клеток являющихся низиной (частный случай, когда 1 клетка)
 def isBottom(map, x, y, route):
     point = {'x': x, 'y': y}
-    
+
+    # Точка останова рекурсии
     if point in route:
         return True
-    
+
+    # Добавляем точку в историю посещения
     route.append(point)
-    
+
+    # Проверка на пограничные значения (около моря)
     if(x == 0 or x == map.shape[0] - 1): return False
     if(y == 0 or y == map.shape[1] - 1): return False
-    
+
+    # Проверка соседей точки
+    # Если есть точка одинаковой высоты, то проверяем ее рекурентно
+    # Если есть соседи ниже точки (рекурентно) => прерываем проверку (False)
     if(map[x][y] == map[x + 1][y] and 'right' not in route and isBottom(map, x + 1, y, route) == False):
         return False
     if(map[x][y] == map[x][y - 1] and 'down' not in route and isBottom(map, x, y - 1, route) == False):
@@ -25,8 +32,13 @@ def isBottom(map, x, y, route):
         return False
     if(map[x][y] == map[x][y + 1] and 'up' not in route and isBottom(map, x, y + 1, route) == False):
         return False
-    
-    if(map[x][y] <= map[x + 1][y] and 
+
+
+    # Если мы прошли проверки выше, значит
+    # точки равные нашей нам подходят
+    # проверяем, что все соседи хотя бы не больше
+    # возвращаем весь путь, проделанный рекурсией
+    if(map[x][y] <= map[x + 1][y] and
        map[x][y] <= map[x - 1][y] and
        map[x][y] <= map[x][y + 1] and 
        map[x][y] <= map[x][y - 1]):
@@ -65,16 +77,24 @@ def fillCells(map, amount, points):
 
 # Главный метод для подсчета осадков
 def rain(island):
-    rain_amount = 0
-    old_sum = 0
+    rain_amount = 0 # кол-во осадков
+    old_sum = 0     # переменная для отслеживания изменений на острове
+
+    # Пока сумма высот на острове меняется
     while sum(sum(island)) != old_sum:
+
+        # Записываем новую сумму высот
         old_sum = sum(sum(island))
+
+        # Пробегаем по клеткам острова последовательно
         for x in range(island.shape[0]):
             for y in range(island.shape[1]):
+                # Проверяем состояние ячейки
                 res = isBottom(island, x, y, [])
+                # Если ячейка - низменность
                 if(res):
-                    amount = getMinimumAmount(island, res)
-                    rain_amount += fillCells(island, amount, res)
+                    amount = getMinimumAmount(island, res)          # Находим максимум, который мы можем добавить
+                    rain_amount += fillCells(island, amount, res)   # Заполняем ячейки, добавляем результат в rain_amount
 
     return rain_amount
 
--- sequence/sequence.py
+++ sequence/sequence.py
@@ -4,30 +4,44 @@ from itertools import count
 
 # Посимвольный генератор строки состоящий из последовательных чисел
 def char_generator():
+    # Перечисляем последовательно числа
     for n in count():
+        # Пробегаем по каждой букве в числе
         for char in str(n):
             yield char
 
 # Найти первое вхождение
 def find(number):
-    number = str(number)
-    generator = char_generator()
+    number = str(number)            # Число
+    generator = char_generator()    # Генератор последовательности
     
     index = 0
+    # Посимвольно
     for current_char in generator:
+        # Флаг для отслеживания состояния поиска подстроки
         complete = True
+        # Пробегаем по символам нашего числа
         for number_char in number:
+            # Если наш очередной символ равен символу генератора
             if current_char == number_char:
+                # Генерируем следующий и продолжаем цикл
                 current_char = generator.next()
                 index += 1
+            # Иначе символы отличаются, вхождения нет
             else:
+                # Устанавливаем флаг поиска в False
                 complete = False
                 break
-                
+
+        # Если флаг не был переключен в False (вхождение не прервано)
+        # Возвращаем индекс вхождения, прерываем функцию
         if complete == True:
             return index - len(number)
-        
+
         index += 1
+
+
+
 
 stdout = open("stdout", "r+")
 stdout.seek(0)
